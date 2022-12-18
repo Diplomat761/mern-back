d984ab972bdfdddcdecc3643d4aa44b7d52502f1
Diplomat761 <l7693744@gmail.com>
2022-12-19 00:35:17 +0700

    update_6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 
 import mongoose from 'mongoose';
 
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 
@@ -45,7 +47,10 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     });
 });
 
-app.get('/posts', PostController.getAll);                           
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll); 
+app.get('/posts/tags', PostController.getLastTags);                          
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
